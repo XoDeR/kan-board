6a0e5b1c89f8f564daf6884bbd20be19a8c9f870
Volodymyr <XoDeR@users.noreply.github.com>
2023-10-28 18:25:58 +0200

    Added avatar from react-avatar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,14 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import Avatar from "react-avatar";
 
 const Header = () => {
   return (
     <header>
-      <div className="flex">
+      <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
         <Image
           className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
           src="/logo.png"
@@ -13,7 +16,7 @@ const Header = () => {
           width={300}
           height={100}
         ></Image>
-        <div>
+        <div className="flex">
           {/* Search box */}
           <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
             <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
@@ -27,6 +30,7 @@ const Header = () => {
             </button>
           </form>
           {/* Avatar */}
+          <Avatar name="Victor Codeberg" />
         </div>
       </div>
     </header>
