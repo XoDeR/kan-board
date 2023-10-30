c65a23a7c12148832f4d76d313b6c0d869cf8f74
Volodymyr <XoDeR@users.noreply.github.com>
2023-10-30 12:15:16 +0100

    added styling to columns' headers

--- components/Column.tsx
+++ components/Column.tsx
@@ -7,9 +7,15 @@ type Props = {
   index: number;
 };
 
-const idToColumnText: {};
-
 const Column = ({ id, todos, index }: Props) => {
+  const idToColumnText: {
+    [key in TypeColumn]: string;
+  } = {
+    todo: "To Do",
+    in_progress: "In Progress",
+    done: "Done",
+  };
+
   return (
     <Draggable draggableId={id} index={index}>
       {(provided) => (
@@ -28,7 +34,12 @@ const Column = ({ id, todos, index }: Props) => {
                   snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"
                 }`}
               >
-                <h2>{id}</h2>
+                <h2 className="flex justify-between font-bold text-xl p-2">
+                  {idToColumnText[id]}
+                  <span className="text-gray-500 bg-gray-200 rounded-full px-2 py-2 text-sm font-normal">
+                    {todos.length}
+                  </span>
+                </h2>
               </div>
             )}
           </Droppable>
