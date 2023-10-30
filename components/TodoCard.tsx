2ea5c3678c6f258f6d6cd48bc983243dccd643ac
Volodymyr <XoDeR@users.noreply.github.com>
2023-10-30 13:21:45 +0100

    todo card and styling

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { XCircleIcon } from "@heroicons/react/24/solid";
 import {
   DraggableProvidedDragHandleProps,
   DraggableProvidedDraggableProps,
@@ -29,7 +30,13 @@ const TodoCard = ({
       {...dragHandleProps}
       ref={innerRef}
     >
-      <h1>Card</h1>
+      <div className="flex justify-between items-center p-5">
+        <p>{todo.title}</p>
+        <button className="text-red-500 hover:tet-red-600">
+          <XCircleIcon className="ml-5 h-8 w-8" />
+        </button>
+      </div>
+      {/* TODO: add image here */}
     </div>
   );
 };
