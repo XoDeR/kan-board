dc82e5862521fee80f85dd0f09c9ad2425b1dc72
Volodymyr <XoDeR@users.noreply.github.com>
2023-10-30 12:57:31 +0100

    added dnd behavior for a todo card

---
+++ components/TodoCard.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import {
+  DraggableProvidedDragHandleProps,
+  DraggableProvidedDraggableProps,
+} from "react-beautiful-dnd";
+
+type Props = {
+  todo: Todo;
+  index: number;
+  id: TypeColumn;
+  innerRef: (element: HTMLElement | null) => void;
+  draggableProps: DraggableProvidedDraggableProps;
+  dragHandleProps: DraggableProvidedDragHandleProps;
+};
+
+const TodoCard = ({
+  todo,
+  index,
+  id,
+  innerRef,
+  draggableProps,
+  dragHandleProps,
+}: Props) => {
+  return (
+    <div
+      className="bg-white rounded-md space-y-2 drop-shadow-md"
+      {...draggableProps}
+      {...dragHandleProps}
+      ref={innerRef}
+    >
+      <h1>Card</h1>
+    </div>
+  );
+};
+
+export default TodoCard;
