682108bc4b82e195bb902900d8abbd7f1d574e86
Volodymyr <XoDeR@users.noreply.github.com>
2023-10-31 20:30:07 +0100

    dnd logic implemented!

--- components/Board.tsx
+++ components/Board.tsx
@@ -29,6 +29,60 @@ const Board = () => {
       const rearrangedColumns = new Map(entries);
       setBoardState({ ...board, columns: rearrangedColumns });
     }
+
+    // card drag
+    const columns = Array.from(board.columns);
+    const startColIndex = columns[Number(source.droppableId)];
+    const finishColIndex = columns[Number(destination.droppableId)];
+
+    const startCol: Column = {
+      id: startColIndex[0],
+      todos: startColIndex[1].todos,
+    };
+
+    const finishCol: Column = {
+      id: finishColIndex[0],
+      todos: finishColIndex[1].todos,
+    };
+
+    if (!startCol || !finishCol) return;
+
+    if (source.index === destination.index && startCol === finishCol) return;
+
+    const newTodos = startCol.todos;
+    const [todoMoved] = newTodos.splice(source.index, 1);
+
+    if (startCol.id === finishCol.id) {
+      // same column task drag
+      newTodos.splice(destination.index, 0, todoMoved);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+      const newColumns = new Map(board.columns);
+      newColumns.set(startCol.id, newCol);
+
+      // TODO update db
+
+      setBoardState({ ...board, columns: newColumns });
+    } else {
+      // dragging to another column
+      const finishTodos = Array.from(finishCol.todos);
+      finishTodos.splice(destination.index, 0, todoMoved);
+
+      const newColumns = new Map(board.columns);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+
+      newColumns.set(startCol.id, newCol);
+      newColumns.set(finishCol.id, { id: finishCol.id, todos: finishTodos });
+
+      // TODO update db
+
+      setBoardState({ ...board, columns: newColumns });
+    }
   };
 
   return (
