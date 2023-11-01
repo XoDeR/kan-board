77acf72e951c3c69026a86409db0182eeb531c12
Volodymyr <XoDeR@users.noreply.github.com>
2023-11-01 12:11:49 +0100

    added state for search

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,14 @@ import Image from "next/image";
 import React from "react";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Avatar from "react-avatar";
+import { useBoardStore } from "@/store/BoardStore";
 
 const Header = () => {
+  const [searchString, setSearchString] = useBoardStore((state) => [
+    state.searchString,
+    state.setSearchString,
+  ]);
+
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
@@ -29,6 +35,8 @@ const Header = () => {
               className="flex-1 outline-none p-2"
               type="text"
               placeholder="Search"
+              value={searchString}
+              onChange={(e) => setSearchString(e.target.value)}
             />
             <button type="submit" hidden>
               Search
