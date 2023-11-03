e85b37c0b2265747f3c07b44715229cbf24d807a
Volodymyr <XoDeR@users.noreply.github.com>
2023-11-03 12:34:08 +0100

    Added headlessui

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-[#cfccc1]">
         {children}
-        {/* <Modal /> */}
+        <Modal />
       </body>
     </html>
   );
